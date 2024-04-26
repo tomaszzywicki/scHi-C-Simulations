ad19985b1171975634c72f02ae2adce621ad9cbf
Kornel <kornel.tlaczala@gmail.com>
2024-04-26 20:23:18 +0200

    create SARW class

---
+++ km3/model_init.py
@@ -0,0 +1,5 @@
+class SARW:
+    def __init__(self, node_count, grid_size):
+        self.n = node_count
+        self.grid_size = grid_size
+
---
+++ km3/test.py
@@ -0,0 +1,4 @@
+import model_init
+
+walk = model_init.SARW(1000, 10)
+print(walk.n, walk.grid_size)

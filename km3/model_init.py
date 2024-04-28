091a891e544c0b403dcf89ef33a8c91bc5f66cff
Kornel <kornel.tlaczala@gmail.com>
2024-04-28 19:28:06 +0200

    implement Grid and Field classes

--- km3/model_init.py
+++ km3/model_init.py
@@ -13,23 +13,23 @@ class RandomWalker(ABC):
 
         self.node_count = node_count
         self.grid_size = grid_size
-        self.path = self.create_path()
+        self.walk = self.create_walk()
 
     def __str__(self):
         return f"Self Avoiding Random Walk:\n\tnode_count = {self.node_count}\n\tgrid_size = {self.grid_size}"
 
     @abstractmethod
-    def create_path(self):
+    def create_walk(self):
         pass
 
-    def get_coords(self, path):
-        x = [pos[0] for pos in path]
-        y = [pos[1] for pos in path]
-        z = [pos[2] for pos in path]
+    def get_coords(self, walk):
+        x = [pos[0] for pos in walk]
+        y = [pos[1] for pos in walk]
+        z = [pos[2] for pos in walk]
         return x, y, z
 
-    def plot_path(self):
-        x, y, z = self.get_coords(self.path)
+    def plot(self):
+        x, y, z = self.get_coords(self.walk)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
         ax.plot(x, y, z)
@@ -44,26 +44,26 @@ class RandomWalker(ABC):
     def get_ratio(self, a, b):
         return round(a / b * 100, 2)
 
-    def path_completeness_ratio(self):
-        return self.get_ratio(len(self.path), self.node_count)
+    def get_walk_completeness(self):
+        return self.get_ratio(len(self.walk), self.node_count)
 
     def show_info(self):
         print(f"should be filled in {self.get_ratio(self.node_count, self.get_grid_volume())}%")
-        print(f"has been filled in {self.get_ratio(len(self.path), self.get_grid_volume())}%")
-        print(f"the path is {self.path_completeness_ratio()}% complete")
+        print(f"has been filled in {self.get_ratio(len(self.walk), self.get_grid_volume())}%")
+        print(f"the walk is {self.get_walk_completeness()}% complete")
 
 class SARW(RandomWalker):
 
     def __init__(self, node_count, grid_size):
         super().__init__(node_count, grid_size)
 
-    def create_path(self):
+    def create_walk(self):
 
         MOVES = np.array([(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)])
 
         visited = set()
         pos = (0, 0, 0)
-        path = [pos]
+        walk = [pos]
 
         for i in range(self.node_count):
             move = random.choice(MOVES)
@@ -76,20 +76,59 @@ class SARW(RandomWalker):
                 continue
             visited.add(new_pos)
             pos = new_pos
-            path.append(pos)
+            walk.append(pos)
 
-        return path
+        return walk
 
 class AdvancedSARW(RandomWalker):
 
     def __init__(self, node_count, grid_size):
         super().__init__(node_count, grid_size)
 
-    def create_path(self):
+    def create_walk(self):
         
         shape = np.repeat(self.grid_size*2+1, 3)
-        visited = np.full(shape, False, dtype=bool)
         
-        current_position = (0,0,0)
-        visited[current_position] = True
-        print(visited)
+
+class Grid():
+
+    def __init__(self, size):
+        self.size = size
+        self.filled_fields = 0
+
+        n = size*2+1
+        self.visited = np.full((n, n, n), False, dtype=bool)
+
+    def is_valid(self, field):
+        if field.x >= abs(self.size):
+            return False
+        if field.y >= abs(self.size):
+            return False
+        if field.z >= abs(self.size):
+            return False
+        return True
+    
+    def is_empty(self, field):
+        return not self.visited[field.x, field.y, field.z]
+
+    def fill_field(self, field):
+        self.visited[field.x, field.y, field.z] = True
+        self.filled_fields += 1
+
+    def empty_field(self, field):
+        self.visited[field.x, field.y, field.z] = False
+        self.filled_fields -= 1
+
+    
+
+class Field():
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def get_neighbours_at_distance(self, distance):
+        #   TODO
+        #   implement
+        pass
